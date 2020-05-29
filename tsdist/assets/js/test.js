63d4ed72e565de5079899f1d28cf373976fa843c
GZYZG <2227423992@qq.com>
2020-05-30 02:35:42 +0800

    删除一些没用的文件

--- tsdist/assets/js/test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Dom_1 = require("../../app/commons/Dom");
-function frame() {
-    Dom_1.genFrame(); //console.log("hahhahahahhhahahhaahhaha");
-}
