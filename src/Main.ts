61e84b896dc1f327fc4653f3ddb67e48e2f3f19b
GuoZhiyan <34331055+GZYZG@users.noreply.github.com>
2020-06-10 19:48:46 +0800

    Update Main.ts

--- src/Main.ts
+++ src/Main.ts
@@ -1,3 +1,5 @@
 import { Application } from './app/application'
 
+window.APPINFO = "author: 郭治焱\nappname: 面向金丝猴社群的亲缘结构研究\ndate: 2020-05-28\ncomment: 本项目源自作者的本科毕设项目，与西北大生命科学学院合作的一个项目\ngit: https://github.com/GZYZG/Three"
+
 new Application();
